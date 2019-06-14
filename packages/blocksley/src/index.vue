ec4514db0090a6de8fce30c205d91c2332808d54
Kurtis Fields <kurtisfields@gmail.com>
2019-06-14 12:53:05 -0400

    :delay="250"

--- packages/blocksley/src/index.vue
+++ packages/blocksley/src/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <draggable v-model="blocks"
-      :touchStartThreshold="16"
+      :delay="250"
       handle=".grippy"
     >
       <frame v-for="block in blocks" :key="block.id" :model="block" @action="onAction" @active="onActive"/>
