f17d48c9f6c3d10bed0b0f1d221e399a3250e97c
Kurtis Fields <kurtisfields@gmail.com>
2019-06-17 14:04:06 -0400

    sigh

--- packages/blocksley/src/components/EFrame.js
+++ packages/blocksley/src/components/EFrame.js
@@ -2,7 +2,7 @@ import kits from 'blocksley/kits'
 
 export default {
   template: `
-  <div>
+  <div v-clickaway="deactivate">
     <component v-bind:is="vu" :frame="this" :model="model" :class="model.class" @action="this.onAction" @active="this.onActive"/>
   </div>`,
   props: ['node', 'updateAttrs', 'view', 'getPos'],
@@ -51,6 +51,7 @@ export default {
       this.vu = kits[this.model.type].editor
     },
     deactivate () {
+      console.log('deactivate')
       this.isActive = false
       this.vu = kits[this.model.type].viewer
     }
