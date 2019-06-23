bd3234de0d78241ae2e151551a30c469db4129aa
Kurtis Fields <kurtisfields@gmail.com>
2019-06-23 01:17:25 -0400

    fix blur problem

--- packages/blocksley/src/plugins/MenuBubble.js
+++ packages/blocksley/src/plugins/MenuBubble.js
@@ -1,71 +1,17 @@
 import { Plugin, PluginKey } from 'prosemirror-state'
 
-function textRange(node, from, to) {
-  const range = document.createRange()
-  range.setEnd(node, to == null ? node.nodeValue.length : to)
-  range.setStart(node, from || 0)
-  return range
-}
-
-function singleRect(object, bias) {
-  const rects = object.getClientRects()
-  return !rects.length ? object.getBoundingClientRect() : rects[bias < 0 ? 0 : rects.length - 1]
-}
-
-function coordsAtPos(view, pos, end = false) {
-  const { node, offset } = view.docView.domFromPos(pos)
-  let side
-  let rect
-  if (node.nodeType === 3) {
-    if (end && offset < node.nodeValue.length) {
-      rect = singleRect(textRange(node, offset - 1, offset), -1)
-      side = 'right'
-    } else if (offset < node.nodeValue.length) {
-      rect = singleRect(textRange(node, offset, offset + 1), -1)
-      side = 'left'
-    }
-  } else if (node.firstChild) {
-    if (offset < node.childNodes.length) {
-      const child = node.childNodes[offset]
-      rect = singleRect(child.nodeType === 3 ? textRange(child) : child, -1)
-      side = 'left'
-    }
-    if ((!rect || rect.top === rect.bottom) && offset) {
-      const child = node.childNodes[offset - 1]
-      rect = singleRect(child.nodeType === 3 ? textRange(child) : child, 1)
-      side = 'right'
-    }
-  } else {
-    rect = node.getBoundingClientRect()
-    side = 'left'
-  }
-
-  const x = rect[side]
-
-  return {
-    top: rect.top,
-    bottom: rect.bottom,
-    left: x,
-    right: x,
-  }
-}
-
-
 class Menu {
 
   constructor({ options, editorView }) {
     this.options = {
       ...{
         element: null,
-        keepInBounds: true,
         onUpdate: () => false,
       },
       ...options,
     }
     this.editorView = editorView
     this.isActive = false
-    this.left = 0
-    this.bottom = 0
     // new stuff
     console.log('MenuBubble')
     const el = this.options.element
@@ -86,7 +32,7 @@ class Menu {
     })
 
     this.options.editor.on('blur', ({ event }) => {
-      this.hide(event)
+      // this.hide(event)
     })
   }
 
@@ -115,9 +61,7 @@ class Menu {
 
   sendUpdate() {
     this.options.onUpdate({
-      isActive: this.isActive,
-      //left: this.left,
-      //bottom: this.bottom,
+      isActive: this.isActive
     })
   }
 
