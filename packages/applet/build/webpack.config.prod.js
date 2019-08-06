fcafae4241cca0b1c0c944b28f0786b91070afd1
Kurtis Fields <kurtisfields@gmail.com>
2019-08-06 17:39:02 -0400

    custom dist build for applet

---
+++ packages/applet/build/webpack.config.prod.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.config.base')
+const MiniCssExtractPlugin  = require('mini-css-extract-plugin')
+
+module.exports = merge(baseConfig, {
+  mode: 'production',
+  /*
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendor",
+          chunks: "all",
+        },
+      },
+    },
+  }, */
+  module: {
+    rules: [
+      {
+        test: /\.css?$/,
+        use: [
+          MiniCssExtractPlugin.loader, 
+          'css-loader'
+        ]
+      }, {
+        test: /\.styl(us)?$/,
+        use: [
+          MiniCssExtractPlugin.loader, 
+          'css-loader', 
+          'stylus-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+      chunkFilename: "css/[name][id].css"
+    })
+  ]
+})
